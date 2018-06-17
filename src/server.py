569730715a129124e606ffddd127b5ba6f72253c
Rich Yonts <ryonts@iMac5k.local>
2018-06-16 22:11:41 -0700

    added sending mined blocks to other nodes

--- src/server.py
+++ src/server.py
@@ -1,5 +1,7 @@
 from json import dumps, loads
 from time import time
+from requests import post
+from socket import gethostbyname, gethostname
 
 from src.block import Block
 from src.blockchain import Blockchain
@@ -11,6 +13,7 @@ handling the details of the endpoints specified by a node in the TrustNet.
 
 class Server:
     def __init__(self, nodes):
+        self.nodes = nodes
         self.transactions = {}
         self.blockchain = Blockchain(nodes)
         
@@ -25,9 +28,12 @@ class Server:
         data = loads(request.json())
         block = Block(data['index'], data['transactions'], data['timestamp'], data['previousHash'])
         proof = data['hash']
-        newBlock = self.blockchain.addBlock(block, proof)
-        if not newBlock:
+        added = self.blockchain.addBlock(block, proof)
+        if not added:
             return 'block discarded by node', 409
+        processedTransactions = data['transactions']
+        for transaction in processedTransactions:
+            del self.transactions[transaction['id']]
         return 'block added to chain', 202
 
     def readSingleBlock(self, index):
@@ -40,6 +46,14 @@ class Server:
         newBlock = Block(self.blockchain.lastBlock.index + 1, self.transactions, time(), self.blockchain.lastBlock.currentHash)
         proof = self.blockchain.proofOfWork(newBlock)
         self.blockchain.addBlock(newBlock, proof)
+        local = gethostbyname(gethostname())
+        for node in self.nodes:
+            h, p = node.split(':')
+            if h in local:
+                url = 'http://localhost:' + p + '/block'
+            else:
+                url = 'http://{}/block'.format(node)
+            post(url, data=dumps(newBlock.__dict__, sort_keys=True), headers={ 'Content-type':'application/json' })
         self.transactions = {}
         return 'new block created and added to chain', 201
 
